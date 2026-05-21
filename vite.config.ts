d7097ed1d5e47a171ecd97d7338fc7d6d745cbd4
Kobayashi24730 <ajenteduplo000@gmail.com>
2026-05-21 10:45:14 -0300

    Melhorias no vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/dev-journey-art/",
+  base: "/My_portifolio_git/",
   server: {
     host: "::",
     port: 8080,
